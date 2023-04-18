a072a1cbbd36d78fd354619fc8a9a7f054a83304
Rowoon Lee <dev.rowoon@gmail.com>
2023-04-18 20:19:42 +0900

    [feat] 해당 글로 가는 url 생성

--- app/list/page.js
+++ app/list/page.js
@@ -1,30 +1,22 @@
 import { connectDB } from "@/util/database";
+import Link from "next/link";
 
 export default async function List() {
   const db = (await connectDB).db("forum");
   let result = await db.collection("post").find().toArray();
+  console.log(result);
   return (
     <div className="list-bg">
       {result.map((a, i) => {
         return (
           <div className="list-item" key={i}>
             <h4>{result[i].title}</h4>
+            {/* 문자와 자료형을 합칠때는 +를 사용하자!!! */}
+            <Link href={"/detail/" + result[i]._id}>링크</Link>
             <p>1월 1일</p>
           </div>
         );
       })}
-      {/* <div className="list-item">
-        <h4>{result[0].title}</h4>
-        <p>1월 1일</p>
-      </div>
-      <div className="list-item">
-        <h4>{result[1].title}</h4>
-        <p>1월 1일</p>
-      </div>
-      <div className="list-item">
-        <h4>{result[2].title}</h4>
-        <p>1월 1일</p>
-      </div> */}
     </div>
   );
 }
