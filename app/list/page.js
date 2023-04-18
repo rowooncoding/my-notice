e7f65c560f8d9b01135e4a6f6becad8f357b6af5
Rowoon Lee <dev.rowoon@gmail.com>
2023-04-18 16:47:15 +0900

    [docs] 리스트 페이지 레이아웃 [style] 글로벌 css 스타일 정의

---
+++ app/list/page.js
@@ -0,0 +1,18 @@
+export default async function List() {
+  return (
+    <div className="list-bg">
+      <div className="list-item">
+        <h4>글제목</h4>
+        <p>1월 1일</p>
+      </div>
+      <div className="list-item">
+        <h4>글제목</h4>
+        <p>1월 1일</p>
+      </div>
+      <div className="list-item">
+        <h4>글제목</h4>
+        <p>1월 1일</p>
+      </div>
+    </div>
+  );
+}
