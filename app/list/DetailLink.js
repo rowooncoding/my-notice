54a4396dd297701851c3b57ef4263919e5224252
Rowoon Lee <dev.rowoon@gmail.com>
2023-04-18 20:23:54 +0900

    [학습] 페이지 넘어가기 학습을 위한 임시 컴포넌트 생성

---
+++ app/list/DetailLink.js
@@ -0,0 +1,5 @@
+"use client";
+
+export default function DetailLink() {
+  return <button onClick={() => {}}>버튼</button>;
+}
--- app/list/page.js
+++ app/list/page.js
@@ -4,7 +4,6 @@ import Link from "next/link";
 export default async function List() {
   const db = (await connectDB).db("forum");
   let result = await db.collection("post").find().toArray();
-  console.log(result);
   return (
     <div className="list-bg">
       {result.map((a, i) => {
