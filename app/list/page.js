79f87524e1202aa0fbd658c37ecf7e0a5a5dbea9
Rowoon Lee <dev.rowoon@gmail.com>
2023-04-18 17:00:09 +0900

    [feat] 데이터에 저장된 제목 출력하기

--- app/list/page.js
+++ app/list/page.js
@@ -1,16 +1,21 @@
+import { connectDB } from "@/util/database";
+
 export default async function List() {
+  const db = (await connectDB).db("forum");
+  let result = await db.collection("post").find().toArray();
+  console.log(result);
   return (
     <div className="list-bg">
       <div className="list-item">
-        <h4>글제목</h4>
+        <h4>{result[0].title}</h4>
         <p>1월 1일</p>
       </div>
       <div className="list-item">
-        <h4>글제목</h4>
+        <h4>{result[1].title}</h4>
         <p>1월 1일</p>
       </div>
       <div className="list-item">
-        <h4>글제목</h4>
+        <h4>{result[2].title}</h4>
         <p>1월 1일</p>
       </div>
     </div>
