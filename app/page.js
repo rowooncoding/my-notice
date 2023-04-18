a5a7a679fb8dfb2f6faef187c4762713eeaf502b
Rowoon Lee <dev.rowoon@gmail.com>
2023-04-18 16:42:30 +0900

    [feat] 코드 수정

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import { connectDB } from "@/util/database";
 
 export default async function Home() {
   // const client = await connectDB;
-  const db = (await connetDB).db("forum");
+  const db = (await connectDB).db("forum");
   //포스트에 있는 모든 데이터를 가져와서 array로 변환해주세요
   await db.collection("post").find().toArray();
   return <div></div>;
