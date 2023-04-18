7b134fc5e46c467c67294f8d0e2a985acdf530f0
Rowoon Lee <dev.rowoon@gmail.com>
2023-04-18 16:25:46 +0900

    [feat] 몽고db 연결 완료

--- app/page.js
+++ app/page.js
@@ -1,6 +1,12 @@
 import { MongoClient } from "mongodb";
 
-export default function Home() {
-  MongoClient.connect();
+export default async function Home() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://zoda05:qhdks367@rowoon.vfl045x.mongodb.net/?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+    }
+  );
+  const db = client.db("forum");
   return <div></div>;
 }
