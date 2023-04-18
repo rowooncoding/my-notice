ac058fb80a05b791688a7e6a640b0f245450e6a1
Rowoon Lee <dev.rowoon@gmail.com>
2023-04-18 16:33:47 +0900

    [feat] 컬렉션의 모든 document 꺼내오기

--- app/page.js
+++ app/page.js
@@ -4,5 +4,7 @@ import { MongoClient } from "mongodb";
 export default async function Home() {
   const client = await connectDB;
   const db = client.db("forum");
+  //포스트에 있는 모든 데이터를 가져와서 array로 변환해주세요
+  await db.collection("post").find().toArray();
   return <div></div>;
 }
