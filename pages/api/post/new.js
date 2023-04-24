bd3205fd178455dfb08e35c0e85efc22b4f34e57
Rowoon Lee <dev.rowoon@gmail.com>
2023-04-24 16:53:39 +0900

    [feat] validation 넣기

--- pages/api/post/new.js
+++ pages/api/post/new.js
@@ -2,8 +2,12 @@ import { connectDB } from "@/util/database";
 
 export default async function handler(요청, 응답) {
   if (요청.method == "POST") {
-    let db = (await connectDB).db("forum");
-    let result = db.collection("post").insertOne(요청.body);
-    응답.redirect(302, "/list");
+    if (요청.body.title == "") {
+      응답.status(500).json("제목을 쓰세요");
+    } else {
+      let db = (await connectDB).db("forum");
+      let result = db.collection("post").insertOne(요청.body);
+      응답.redirect(302, "/list");
+    }
   }
 }
