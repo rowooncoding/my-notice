09ed7844d6804663156e371095d0499377458a57
Rowoon Lee <dev.rowoon@gmail.com>
2023-04-24 15:57:41 +0900

    [docs] input 생성

--- app/write/page.js
+++ app/write/page.js
@@ -3,6 +3,8 @@ export default function Write() {
     <div>
       <h4>글작성</h4>
       <form action="/api/test" method="POST">
+        <input name="title" />
+        <input name="content" />
         <button type="submit">버튼</button>
       </form>
     </div>
