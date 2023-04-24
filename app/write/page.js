25f996770ec3a4c99c2bd23e0469d34942336b90
Rowoon Lee <dev.rowoon@gmail.com>
2023-04-24 16:52:33 +0900

    [docs]page.js 오타수정

--- app/write/page.js
+++ app/write/page.js
@@ -1,11 +1,10 @@
-export default function Write() {
+export default async function Write() {
   return (
     <div className="p-20">
-      <h4>글작성</h4>
-      <form action="/api/test/new" method="POST">
+      <form action="/api/post/new" method="POST">
         <input name="title" placeholder="글제목" />
         <input name="content" placeholder="글내용" />
-        <button type="submit">버튼</button>
+        <button type="submit">전송</button>
       </form>
     </div>
   );
