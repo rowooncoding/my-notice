5feb8e443f1918dcd40883b5537dd3b23d3d6f58
Rowoon Lee <dev.rowoon@gmail.com>
2023-04-24 16:54:16 +0900

    [docs] else문 삭제

--- pages/api/post/new.js
+++ pages/api/post/new.js
@@ -4,10 +4,9 @@ export default async function handler(요청, 응답) {
   if (요청.method == "POST") {
     if (요청.body.title == "") {
       응답.status(500).json("제목을 쓰세요");
-    } else {
-      let db = (await connectDB).db("forum");
-      let result = db.collection("post").insertOne(요청.body);
-      응답.redirect(302, "/list");
     }
+    let db = (await connectDB).db("forum");
+    let result = db.collection("post").insertOne(요청.body);
+    응답.redirect(302, "/list");
   }
 }
