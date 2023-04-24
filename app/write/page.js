8b968284960ec78711b7b854afb4eecab406e248
Rowoon Lee <dev.rowoon@gmail.com>
2023-04-24 16:00:13 +0900

    [docs] input태그 마무리 [style] 디자인

--- app/write/page.js
+++ app/write/page.js
@@ -1,10 +1,10 @@
 export default function Write() {
   return (
-    <div>
+    <div className="p-20">
       <h4>글작성</h4>
       <form action="/api/test" method="POST">
-        <input name="title" />
-        <input name="content" />
+        <input name="title" placeholder="글제목" />
+        <input name="content" placeholder="글내용" />
         <button type="submit">버튼</button>
       </form>
     </div>
