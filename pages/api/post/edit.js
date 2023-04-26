9c83ecabc86ebdb4b99e5cd2fba85e0d5a23d65b
Rowoon Lee <dev.rowoon@gmail.com>
2023-04-26 16:26:56 +0900

    [feat] 수정코드 작성

--- pages/api/post/edit.js
+++ pages/api/post/edit.js
@@ -1,10 +1,13 @@
 import { connectDB } from "@/util/database";
+import { ObjectId } from "mongodb";
 
 export default async function handler(req, res) {
   if (req.method == "POST") {
+    let put = { title: req.body.title, content: req.body.content };
     const db = (await connectDB).db("forum");
     let result = await db
       .collection("post")
-      .updateOne({ _id: 1234 }, { $set: req.body });
+      .updateOne({ _id: new ObjectId(req.body._id) }, { $set: put });
+    res.status(200).redirect("/list");
   }
 }
