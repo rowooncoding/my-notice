65b58ab2b32d1e228e274fb64ea5a05e7c965f0c
Rowoon Lee <dev.rowoon@gmail.com>
2023-04-26 16:02:34 +0900

    [docs] value -> defaultValue

--- app/edit/[id]/page.js
+++ app/edit/[id]/page.js
@@ -13,8 +13,8 @@ export default async function Edit(props) {
     <div className="p-20">
       <h4>수정페이지</h4>
       <form action="/api/post/new" method="POST">
-        <input name="title" value={result.title} />
-        <input name="content" value={result.content} />
+        <input name="title" defaultValue={result.title} />
+        <input name="content" defaultValue={result.content} />
         <button type="submit">전송</button>
       </form>
     </div>
