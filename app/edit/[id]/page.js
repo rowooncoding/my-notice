5b161e64df1b2ca15bc82fdad1868bb1aaa73c87
Rowoon Lee <dev.rowoon@gmail.com>
2023-04-26 16:35:57 +0900

    [fix]Error: Invariant: missing action ID. 수정

--- app/edit/[id]/page.js
+++ app/edit/[id]/page.js
@@ -10,7 +10,7 @@ export default async function Edit(props) {
   return (
     <div className="p-20">
       <h4>수정페이지</h4>
-      <form action="/api/post/new" method="POST">
+      <form action="/api/post/edit" method="POST">
         <input name="title" defaultValue={result.title} />
         <input name="content" defaultValue={result.content} />
         <input
