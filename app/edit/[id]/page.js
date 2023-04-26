f10c5186ddb4bbcdb93c3e4133fa8b4dea480ef3
Rowoon Lee <dev.rowoon@gmail.com>
2023-04-26 15:51:23 +0900

    [feat] props받아와서 글띄우기

--- app/edit/[id]/page.js
+++ app/edit/[id]/page.js
@@ -1,10 +1,18 @@
-export default function Edit() {
+import { connectDB } from "@/util/database";
+import { ObjectId } from "mongodb";
+
+export default async function Edit(props) {
+  const db = (await connectDB).db("forum");
+  let result = await db
+    .collection("post")
+    .findOne({ _id: new ObjectId(props.params.id) });
+  console.log(result);
   return (
     <div className="p-20">
       <h4>수정페이지</h4>
       <form action="/api/post/new" method="POST">
-        <input name="title" placeholder="글제목" />
-        <input name="content" placeholder="글내용" />
+        <input name="title" value={result.title} />
+        <input name="content" value={result.content} />
         <button type="submit">전송</button>
       </form>
     </div>
