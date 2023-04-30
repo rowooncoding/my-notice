ac6f78e8bf005c09c9eabb2190025bd587d56de6
Rowoon Lee <dev.rowoon@gmail.com>
2023-04-30 17:43:04 +0900

    [feat] await 추가

--- pages/api/post/new.js
+++ pages/api/post/new.js
@@ -3,17 +3,16 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "../auth/[...nextauth]";
 
 export default async function handler(요청, 응답) {
-  let session = getServerSession(요청, 응답, authOptions);
-  console.log(session.user.email);
+  let session = await getServerSession(요청, 응답, authOptions);
   if (session) {
     요청.body.author = session.user.email;
   }
-  // if (요청.method == "POST") {
-  //   if (요청.body.title == "") {
-  //     응답.status(500).json("제목을 쓰세요");
-  //   }
-  //   let db = (await connectDB).db("forum");
-  //   let result = db.collection("post").insertOne(요청.body);
-  //   응답.redirect(302, "/list");
-  // }
+  if (요청.method == "POST") {
+    if (요청.body.title == "") {
+      응답.status(500).json("제목을 쓰세요");
+    }
+    let db = (await connectDB).db("forum");
+    let result = db.collection("post").insertOne(요청.body);
+    응답.redirect(302, "/list");
+  }
 }
