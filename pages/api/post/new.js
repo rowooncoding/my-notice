591e517aa769917fa239b50aac754558e01878d1
Rowoon Lee <dev.rowoon@gmail.com>
2023-04-30 17:40:42 +0900

    [feat] 유저 이메일 정보 author에 넣기

--- pages/api/post/new.js
+++ pages/api/post/new.js
@@ -1,12 +1,19 @@
 import { connectDB } from "@/util/database";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]";
 
 export default async function handler(요청, 응답) {
-  if (요청.method == "POST") {
-    if (요청.body.title == "") {
-      응답.status(500).json("제목을 쓰세요");
-    }
-    let db = (await connectDB).db("forum");
-    let result = db.collection("post").insertOne(요청.body);
-    응답.redirect(302, "/list");
+  let session = getServerSession(요청, 응답, authOptions);
+  console.log(session.user.email);
+  if (session) {
+    요청.body.author = session.user.email;
   }
+  // if (요청.method == "POST") {
+  //   if (요청.body.title == "") {
+  //     응답.status(500).json("제목을 쓰세요");
+  //   }
+  //   let db = (await connectDB).db("forum");
+  //   let result = db.collection("post").insertOne(요청.body);
+  //   응답.redirect(302, "/list");
+  // }
 }
