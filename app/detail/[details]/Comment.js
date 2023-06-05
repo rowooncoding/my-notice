db524978af63c6c05629b20bf392e9bc089dd310
Rowoon Lee <dev.rowoon@gmail.com>
2023-06-05 16:23:54 +0900

    댓글완성

--- app/detail/[details]/Comment.js
+++ app/detail/[details]/Comment.js
@@ -4,17 +4,22 @@ import { useEffect, useState } from "react";
 
 export default function Comment(props) {
   let [comment, setComment] = useState("");
+  let [data, setData] = useState([]);
+
   useEffect(() => {
     fetch("/api/comment/list?id=" + props._id)
       .then((r) => r.json())
       .then((result) => {
-        console.log(result);
+        setData(result);
       });
   }, []);
 
   return (
     <div>
       <div>댓글목록</div>
+      {data.length > 0
+        ? data.map((a, i) => <p key={i}>{a.content}</p>)
+        : "댓글없음"}
       <input
         onChange={(e) => {
           setComment(e.target.value);
